b7a0befcb60d924616a1fefc697fc0f12ac057a2
Mar.Gregorios Orthodox Church <126573091+margregorioschurch@users.noreply.github.com>
2023-03-01 16:36:16 +0530

    minify

--- js/readings.js
+++ js/readings.js
@@ -1,141 +1,16 @@
-async function load_readings() {
-	// Use the client's date since the server is always set to Pacific Timezone
-	const today = new Date()
-
-	// JS months are zero-origin, so we have to add 1 to the month
-	const url = `https://orthocal.info/api/gregorian/${today.getFullYear()}/${today.getMonth()+1}/${today.getDate()}/`;
-
-	const response = await fetch(url)
-	if (response.ok) {
-		const day = await response.json()
-
-		// Get an abbreviated subset of the readings
-		abbr_readings = day.abbreviated_reading_indices.map(index => day.readings[index])
-
-		// Use a string template to generate new HTML and add it to the
-		// readings article in the markup.
-		document.querySelector('#readings').innerHTML = `
-			<h2>${today.toDateString()}</h2>
-			<h3>${day.summary_title}</h3>
+async function load_readings(){let n=new Date,t=`https://orthocal.info/api/gregorian/${n.getFullYear()}/${n.getMonth()+1}/${n.getDate()}/`,a=await fetch(t);if(a.ok){let e=await a.json();abbr_readings=e.abbreviated_reading_indices.map(n=>e.readings[n]),document.querySelector("#readings").innerHTML=`
+			<h2>${n.toDateString()}</h2>
+			<h3>${e.summary_title}</h3>
 			<h2>Scripture Readings</h2>
-			${abbr_readings.map(reading => `
-				<h3>${reading.display}</h3>
+			${abbr_readings.map(n=>`
+				<h3>${n.display}</h3>
 				<!-- There is no markup in the verses, so we wrap the whole thing in <p> tags -->
-				<p>${reading.passage.map(verse => verse.content).join(' ')}</p>
-			`).join('')}
+				<p>${n.passage.map(n=>n.content).join(" ")}</p>
+			`).join("")}
 			<h2>Commemorations</h2>
-			${day.stories.map(story => `
-				<h3>${story.title}</h3>
+			${e.stories.map(n=>`
+				<h3>${n.title}</h3>
 				<!-- Stories already have the html markup embedded, so we don't need <p> tags -->
-				${story.story}
-			`).join('')}
-		`
-	}
-}
-
-document.addEventListener('DOMContentLoaded', load_readings);
-
-(function() {
-	$(document ).ready(function() {
-		getVerse();
-	});
-  
-	$("button").click(function(){
-	  getVerse();
-	});
-  
-	var getVerse = function() {
-		$.ajax({
-		  url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
-		  crossDomain: true,
-		  dataType: 'jsonp',
-		  success: function(result){
-			 $("#newQuote0")
-			   .html('<strong>'+
-					 result[0].bookname+
-					 ' ' + result[0].chapter +
-					 '&nbsp;:&nbsp;' + result[0].verse +
-					 '</strong>&nbsp;-&nbsp;' +
-					 result[0].text);
-		  }
-		});
-	}
-})();
-(function() {
-	$(document ).ready(function() {
-		getVerse();
-	});
-  
-	$("button").click(function(){
-	  getVerse();
-	});
-  
-	var getVerse = function() {
-		$.ajax({
-		  url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
-		  crossDomain: true,
-		  dataType: 'jsonp',
-		  success: function(result){
-			 $("#newQuote1")
-			   .html('<strong>'+
-					 result[0].bookname+
-					 ' ' + result[0].chapter +
-					 '&nbsp;:&nbsp;' + result[0].verse +
-					 '</strong>&nbsp;-&nbsp;' +
-					 result[0].text);
-		  }
-		});
-	}
-})();
-(function() {
-	$(document ).ready(function() {
-		getVerse();
-	});
-  
-	$("button").click(function(){
-	  getVerse();
-	});
-  
-	var getVerse = function() {
-		$.ajax({
-		  url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
-		  crossDomain: true,
-		  dataType: 'jsonp',
-		  success: function(result){
-			 $("#newQuote2")
-			   .html('<strong>'+
-					 result[0].bookname+
-					 ' ' + result[0].chapter +
-					 '&nbsp;:&nbsp;' + result[0].verse +
-					 '</strong>&nbsp;-&nbsp;' +
-					 result[0].text);
-		  }
-		});
-	}
-})();
-(function() {
-	$(document ).ready(function() {
-		getVerse();
-	});
-  
-	$("button").click(function(){
-	  getVerse();
-	});
-  
-	var getVerse = function() {
-		$.ajax({
-		  url: "https://labs.bible.org/api/?passage=random&type=json&callback=myCallback", 
-		  crossDomain: true,
-		  dataType: 'jsonp',
-		  success: function(result){
-			 $("#newQuote3")
-			   .html('<strong>'+
-					 result[0].bookname+
-					 ' ' + result[0].chapter +
-					 '&nbsp;:&nbsp;' + result[0].verse +
-					 '</strong>&nbsp;-&nbsp;' +
-					 result[0].text);
-		  }
-		});
-	}
-})();
+				${n.story}
+			`).join("")}
+		`}}document.addEventListener("DOMContentLoaded",load_readings),function(){$(document).ready(function(){n()}),$("button").click(function(){n()});var n=function(){$.ajax({url:"https://labs.bible.org/api/?passage=random&type=json&callback=myCallback",crossDomain:!0,dataType:"jsonp",success:function(n){$("#newQuote0").html("<strong>"+n[0].bookname+" "+n[0].chapter+"&nbsp;:&nbsp;"+n[0].verse+"</strong>&nbsp;-&nbsp;"+n[0].text)}})}}(),function(){$(document).ready(function(){n()}),$("button").click(function(){n()});var n=function(){$.ajax({url:"https://labs.bible.org/api/?passage=random&type=json&callback=myCallback",crossDomain:!0,dataType:"jsonp",success:function(n){$("#newQuote1").html("<strong>"+n[0].bookname+" "+n[0].chapter+"&nbsp;:&nbsp;"+n[0].verse+"</strong>&nbsp;-&nbsp;"+n[0].text)}})}}(),function(){$(document).ready(function(){n()}),$("button").click(function(){n()});var n=function(){$.ajax({url:"https://labs.bible.org/api/?passage=random&type=json&callback=myCallback",crossDomain:!0,dataType:"jsonp",success:function(n){$("#newQuote2").html("<strong>"+n[0].bookname+" "+n[0].chapter+"&nbsp;:&nbsp;"+n[0].verse+"</strong>&nbsp;-&nbsp;"+n[0].text)}})}}(),function(){$(document).ready(function(){n()}),$("button").click(function(){n()});var n=function(){$.ajax({url:"https://labs.bible.org/api/?passage=random&type=json&callback=myCallback",crossDomain:!0,dataType:"jsonp",success:function(n){$("#newQuote3").html("<strong>"+n[0].bookname+" "+n[0].chapter+"&nbsp;:&nbsp;"+n[0].verse+"</strong>&nbsp;-&nbsp;"+n[0].text)}})}}();
