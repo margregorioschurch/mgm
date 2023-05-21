ff4cb2abc8205ee2c9a1c8466be19738a5aa6f78
Mar.Gregorios Orthodox Church <126573091+margregorioschurch@users.noreply.github.com>
2023-05-21 18:58:52 +0530

    Update visitors.js

--- js/visitors.js
+++ js/visitors.js
@@ -1,1 +1,1 @@
-const countEl=document.getElementById("CounterVisitor");function updateVisitCount(){fetch("https://api.countapi.xyz/update/mgm/visits/?amount=1").then(t=>t.json()).then(t=>{countEl.innerHTML=t.value})}updateVisitCount();
+//const countEl=document.getElementById("CounterVisitor");function updateVisitCount(){fetch("https://api.countapi.xyz/update/mgm/visits/?amount=1").then(t=>t.json()).then(t=>{countEl.innerHTML=t.value})}updateVisitCount();
