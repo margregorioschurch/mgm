f401bb7f99190ccec5dd6512ae3cb29f6801f541
Mar.Gregorios Orthodox Church <126573091+margregorioschurch@users.noreply.github.com>
2023-11-04 07:57:57 +0000

    Update visitors.js

--- js/visitors.js
+++ js/visitors.js
@@ -1,5 +1,5 @@
 /* ---- particles.js config ---- */
-
+window.addEventListener('DOMContentLoaded', (event) => {
 particlesJS("particles-js", {
   particles: {
     number: {
@@ -110,7 +110,4 @@ particlesJS("particles-js", {
   },
   retina_detect: true
 });
-
-
-
-//const countEl=document.getElementById("CounterVisitor");function updateVisitCount(){fetch("https://api.countapi.xyz/update/mgm/visits/?amount=1").then(t=>t.json()).then(t=>{countEl.innerHTML=t.value})}updateVisitCount();
+});
