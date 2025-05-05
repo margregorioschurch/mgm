a90eb5936a8d06865e264ec33c2d28ff4f8163d1
Mar.Gregorios Orthodox Church <126573091+margregorioschurch@users.noreply.github.com>
2025-05-06 00:11:35 +0100

    Update visitors.js
    js

--- js/visitors.js
+++ js/visitors.js
@@ -126,7 +126,5 @@ function showLocation(position) {
 }
 
 // Trigger geolocation
-getGeolocation();
-
-  
-//const countEl=document.getElementById("CounterVisitor");function updateVisitCount(){fetch("https://api.countapi.xyz/update/mgm/visits/?amount=1").then(t=>t.json()).then(t=>{countEl.innerHTML=t.value})}updateVisitCount();
+// getGeolocation();
+document.getElementById('acceptCookies').addEventListener('click', getGeolocation);
