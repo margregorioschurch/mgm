27372364e679173d702452c935c810e29cb9ca3e
Your Name <you@example.com>
2025-06-25 10:57:57 +0100

    fix tailwind css

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 // next.config.ts
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repo = 'nextjs'; // Replace with your GitHub repository name
+const repo = 'mgm'; // Replace with your GitHub repository name
 
 /** @type {import('next').NextConfig} */
 const NextConfig = {
