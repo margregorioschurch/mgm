a95325908ea64f57e32aac0c7b0336d26d05c7b8
Your Name <you@example.com>
2025-06-28 15:08:49 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
-// next.config.ts
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repo = 'mgm'; // Replace with your GitHub repository name
+const repo = 'mgm'; // Your GitHub repository name
 
-/** @type {import('next').NextConfig} */
-const NextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
   basePath: isGithubPages ? `/${repo}` : '',
   assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
-export default NextConfig;
+export default nextConfig;
+
