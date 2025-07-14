66972f9094049eee7c5c858fb5d55c23b00c253f
Mar.Gregorios Orthodox Church <126573091+margregorioschurch@users.noreply.github.com>
2025-07-14 16:15:16 +0100

    Update next.config.ts
    eslint ignore

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from 'next';
 
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const isProd = process.env.NODE_ENV === 'production';
 const repo = 'mgm'; // Your GitHub repository name
 
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: isGithubPages ? `/${repo}` : '',
   assetPrefix: isGithubPages ? `/${repo}/` : '',
+};
+
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: isProd,
+  },
 };
 
 export default nextConfig;
